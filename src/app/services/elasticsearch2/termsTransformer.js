0f24aee9bb4f302a13119288672be0f4d20463cf
Lee Elenbaas <lee@gigya-inc.com>
2016-01-28 12:54:21 +0200

    terms transformation
    Commit by Lee Elenbaas <lee@gigya-inc.com>
    On branch: refs/heads/3.0

--- src/app/services/elasticsearch2/termsTransformer.js
+++ src/app/services/elasticsearch2/termsTransformer.js
@@ -1,26 +1,54 @@
 define([
+  'angular',
   'lodash'
 ],
-function (_) {
+function (angular,_) {
   return {
     condition: function(config){
       return config.url.endsWith('/_search') && /^\{\"facets\":\{\"terms\":/.test(config.data);
     },
 
     request: function(config){
-      console.warn({
-        interceptor: 'termsTransformer',
-        outgoing: config
-      });
+      var facetData = angular.fromJson(config.data);
+
+      var aggregationsData = {
+        aggs:{
+            fquery:{
+              filter: facetData.facets.terms.facet_filter.fquery,
+              aggs: {
+                terms: {
+                  terms: {
+                    field: facetData.facets.terms.terms.field
+                  }
+                }
+              }
+          }
+        },
+        size: facetData.size
+      };
+
+      config.data = angular.toJson(aggregationsData);
 
       return config;
     },
-    
+
     response: function(response){
-      console.warn({
-        interceptor: 'termsTransformer',
-        incoming: response
-      });
+      var data = response.data;
+
+      data.facets = {
+        terms: {
+          _type: 'terms',
+          missing: -1, // TODO: figure out where t get the info from
+          total: data.aggregations.fquery.doc_count,
+          other: data.aggregations.fquery.terms.sum_other_doc_count,
+          terms: _.map(data.aggregations.fquery.terms.buckets, function(bucket){
+            return {
+              term: bucket.key,
+              count: bucket.doc_count
+            };
+          })
+        }
+      };
 
       return response;
     }
