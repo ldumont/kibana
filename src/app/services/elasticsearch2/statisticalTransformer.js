f34ab06480187b9c4f434ad150774290480b5737
Lee Elenbaas <lee@gigya-inc.com>
2016-02-04 15:10:25 +0200

    Support stats facets
    Commit by Lee Elenbaas <lee@gigya-inc.com>
    On branch: refs/heads/3.0

--- src/app/services/elasticsearch2/statisticalTransformer.js
+++ src/app/services/elasticsearch2/statisticalTransformer.js
@@ -3,32 +3,37 @@ define([
   'lodash'
 ],
 function (angular,_) {
-  var signature = /^\{\"facets\":\{\"0\":\{\"date_histogram\":/;
+  var signature = /^\{\"facets\":\{\"stats\":\{\"statistical\":/;
 
   return {
     condition: function(config){
-      return false; // TODO:
-      return config.url.endsWith('/_search?search_type=count') && signature.test(config.data);
+      return /\/_search$/.test(config.url) && signature.test(config.data);
     },
 
     request: function(config){
       var facetData = angular.fromJson(config.data);
 
       var aggregationsData = {
-        aggs:{
-            fquery:{
-              filter: facetData.facets.terms.facet_filter.fquery,
-              aggs: {
-                terms: {
-                  terms: {
-                    field: facetData.facets.terms.terms.field
-                  }
-                }
-              }
-          }
-        },
+        aggs:{},
         size: facetData.size
       };
+
+      _.forOwn(facetData.facets, function(num, key){
+        var facet = facetData.facets[key];
+
+        var aggregation = {
+          filter: facet.facet_filter.fquery,
+          aggs: {
+            stats: {
+              extended_stats: {
+                field: facet.statistical.field
+              }
+            }
+          }
+        };
+
+        aggregationsData.aggs[key] = aggregation
+      });
 
       config.data = angular.toJson(aggregationsData);
 
@@ -38,20 +43,24 @@ function (angular,_) {
     response: function(response){
       var data = response.data;
 
-      data.facets = {
-        terms: {
-          _type: 'terms',
-          missing: -1, // TODO: figure out where t get the info from
-          total: data.aggregations.fquery.doc_count,
-          other: data.aggregations.fquery.terms.sum_other_doc_count,
-          terms: _.map(data.aggregations.fquery.terms.buckets, function(bucket){
-            return {
-              term: bucket.key,
-              count: bucket.doc_count
-            };
-          })
-        }
-      };
+      data.facets = {};
+
+      _.forOwn(data.aggregations, function(num, key){
+        var aggregation = data.aggregations[key];
+
+        data.facets[key] = {
+          _type: 'statistical',
+          total: aggregation.doc_count,
+          count: aggregation.stats.count,
+          min: aggregation.stats.min,
+          max: aggregation.stats.max,
+          mean: aggregation.stats.avg,
+          sum: aggregation.stats.sum,
+          sum_of_squares: aggregation.stats.sum_of_squares,
+          variance: aggregation.stats.variance,
+          std_deviation: aggregation.stats.std_deviation
+        };
+      });
 
       return response;
     }
