c3f806b13cad57f2fbc514b8426304adaad81f41
GIGYA\oleksandr.ch <oleksandr.ch@gigya-inc.com>
2017-10-05 10:34:37 +0300

    WIP handle `search_type=count`

--- src/app/services/elasticsearch2/es5Transformer.js
+++ src/app/services/elasticsearch2/es5Transformer.js
@@ -10,6 +10,13 @@ define([],
                 var data = angular.fromJson(config.data);
                 var changed = false;
 
+                // Replace `search_type=count` which is deprecated in ES5
+                if (config.url.indexOf('search_type=count') !== -1) {
+                    changed = true;
+                    config.url = config.url.replace('search_type=count', 'search_type=query_then_fetch');
+                    data.size = 0;
+                }
+
                 // Find and remove (if any) `ignore_unmapped` pram from sort
                 // TODO: not sure if we need to simply delete this? Clarify and refactor if needed
                 if (angular.isArray(data.sort)) {
@@ -34,7 +41,13 @@ define([],
                     data.query.bool = data.query.filtered;
                     delete data.query.filtered;
                 }
-                console.log(data);
+
+
+
+
+
+
+
                 if (changed) {
                     config.data = angular.toJson(data);
                 }
